def5e57488704e1f2bf49bc4327e0f029e8cdeed
jaqueshere <jaques.gillis@gmail.com>
2015-12-24 11:44:10 -0500

    upgraded to owl-carousel-2, fixed carousel divs, added responsive function to JS script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
 	$("#slides").owlCarousel({
 		margin:2,
-		loop: true,
-		items: 3,
-		center: true,
-		nav:true
+		loop: false,
+		items: 1,
+		center: false,
+		responsiveClass:true,
+		responsive: {
+			0: {
+				items:1,
+				nav:true
+			},
+			600: {
+				items:3,
+				nav:true
+			},
+			1000: {
+				items: 3,
+				nav:true
+			}
+		}
 	});
 });
